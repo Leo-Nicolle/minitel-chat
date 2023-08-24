cb2a76c996baa478efae9ba1090df93264a3234b
Leo Nicolle <leo@linkurio.us>
2023-08-24 14:55:44 +0200

    service handle multiple cycles

--- service/minitel.sh
+++ service/minitel.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 SDA= cat /tmp/minitel-plugged.txt
 MNT=''
-while [ ${#SDA} -lt 1 ]
-do
-  SDA=$(cat /tmp/minitel-plugged.txt)
-  sleep 1
-done
-sudo rm /tmp/minitel-plugged.txt
-sudo /sbin/mgetty -br ttyUSB0 4800v23
 
+while [ true ]
+do
+  while [ ${#SDA} -lt 1 ]
+  do
+    SDA=$(cat /tmp/minitel-plugged.txt)
+    sleep 1
+  done
+  sudo rm /tmp/minitel-plugged.txt
+  sudo /sbin/mgetty -br ttyUSB0 4800v23
+  echo "Coucou"
+done
 
